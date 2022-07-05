6d521d0badd42fba8d8e7a0df85a24a6480a1b20
bind-TIAN <50130421+bind-TIAN@users.noreply.github.com>
2022-07-05 14:28:07 +0800

    Delete split-data.py

--- split-data.py
+++
@@ -1,8 +0,0 @@
-import splitfolders
-
-if __name__ == "__main__":
-    base_dir = 'D:/deep-learning-for-image-processing-master/tensorflow_classification/vision_transformer/'
-    splitfolders.ratio(input=base_dir + 'data_set/flower_data/0Z6Jna', output='outputs', seed=2022,
-                       ratio=(0.8, 0.2))
-    # 以上代码将花数据集划分成train和val两个集合
-
